4d9484526d03ade58424d1e720a46a7faefb4d44
Alfredo Martinez <ialfredo.mtz.rmz@gmail.com>
2025-03-31 19:05:17 -0600

    style(nuxtUI): Install, configure NuxtUI, TailwindCSS 4

--- app.vue
+++ app.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <UApp>
+    <main
+      class="md:p-16 mx-auto overflow-auto p-4 print:p-12 relative scroll-my-12 dark:text-neutral-200 text-neutral-800 selection:bg-primary-300 selection:text-primary-900"
+    >
+      Hello from app
+    </main>
+  </UApp>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,30 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
+  experimental: {
+    typedPages: true,
+  },
+  ssr: true,
+  app: {
+    head: {
+      bodyAttrs: {
+        class: 'antialiased h-full',
+      },
+    },
+    pageTransition: { name: 'page', mode: 'out-in' },
+    layoutTransition: { name: 'page', mode: 'out-in' },
+  },
   devtools: {
     enabled: true,
+  },
+  modules: [
+    // https://nuxt.com/modules/ui
+    '@nuxt/ui',
+  ],
+  css: ['~/assets/css/main.css'],
+  colorMode: {
+    preference: 'system',
+    fallback: 'light',
+    disableTransition: false,
   },
 });
