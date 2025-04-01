ae758b4ef935414a5b562c12d2ad90bd5ffd0452
Alfredo Martinez <ialfredo.mtz.rmz@gmail.com>
2025-04-01 14:57:04 -0600

    feat(Navbar): Update Navbar component

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -5,7 +5,9 @@ defineOptions({
 </script>
 
 <template>
-  <header class="flex print:hidden h-16 items-center px-4 sm:px-6 lg:px-8">
+  <header
+    class="sticky top-0 z-10 flex print:hidden h-16 items-center bg-white/75 px-4 backdrop-blur-lg dark:bg-neutral-900/75 sm:px-6 lg:px-8"
+  >
     <div
       class="mx-auto flex w-full max-w-screen-lg items-center justify-end gap-2"
     >
